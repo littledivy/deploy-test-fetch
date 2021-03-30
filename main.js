e5f231b78c9527d4cad646c97a975b158a35eaa0
Divy Srivastava <dj.srivastava23@gmail.com>
2021-03-30 18:31:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const file = await fetch("./example.json");
+const file = await fetch(new URL("./example.json", import.meta.url))
 const contents = await file.json();
 addEventListener("fetch", (event) => {
   event.respondWith(
